99a5f84882bc0fe8d75abac85c3c55e006882b0d
lucas ribeiro guedes <luk_ax@hotmai.com>
2020-08-10 21:46:29 -0300

    1 commit

--- codigo/desafio_iafront/jobs/pedidos/job.py
+++ codigo/desafio_iafront/jobs/pedidos/job.py
@@ -3,9 +3,7 @@ import click
 from datetime import timedelta
 
 from desafio_iafront.data.dataframe_utils import read_csv, read_partitioned_json
-from desafio_iafront.data.saving import save_partitioned
-from desafio_iafront.jobs.pedidos.contants import SAVING_PARTITIONS
-from desafio_iafront.jobs.pedidos.utils import _prepare
+from desafio_iafront.jobs.pedidos.utils import create_save_visita_produto_conversao_df
 
 
 @click.command()
@@ -26,31 +24,10 @@ def main(pedidos, visitas, produtos, saida, data_inicial, data_final):
         hour_partitions = list(range(0, 23))
 
         for hour in hour_partitions:
-            hour_snnipet = f"hora={hour}"
-
-            data_str = data.strftime('%Y-%m-%d')
-            date_partition = f"data={data_str}"
-
-            visitas_partition = os.path.join(visitas, date_partition, hour_snnipet)
-            visitas_df = read_partitioned_json(visitas_partition)
-            visitas_df["product_id"] = visitas_df["product_id"].astype(str)
-            visitas_df["visit_id"] = visitas_df["visit_id"].astype(str)
-
-            pedidos_partition = os.path.join(pedidos, date_partition, hour_snnipet)
-            pedidos_df = read_partitioned_json(pedidos_partition)
-            pedidos_df["visit_id"] = pedidos_df["visit_id"].astype(str)
-
-            visita_com_produto_df = visitas_df.merge(produtos_df, how="inner", on="product_id", suffixes=("", "_off"))
-            visita_com_produto_e_conversao_df = visita_com_produto_df.merge(pedidos_df, how="left",
-                                                                            on="visit_id", suffixes=("", "_off"))
-
-            visita_com_produto_e_conversao_df["data"] = data_str
-            visita_com_produto_e_conversao_df["hora"] = hour
-
-            prepared = _prepare(visita_com_produto_e_conversao_df)
-            save_partitioned(prepared, saida, SAVING_PARTITIONS)
+            date_partition = create_save_visita_produto_conversao_df(data, hour, pedidos, produtos_df, saida, visitas)
             print(f"Concluído para {date_partition} {hour}h")
-
+           
 
 if __name__ == '__main__':
     main()
+    
--- codigo/desafio_iafront/jobs/pedidos/utils.py
+++ codigo/desafio_iafront/jobs/pedidos/utils.py
@@ -1,7 +1,9 @@
 import pandas as pd
-
+import os
 from desafio_iafront.jobs.pedidos.contants import KEPT_COLUNS, COLUMN_RENAMES
-
+from desafio_iafront.data.saving import save_partitioned
+from desafio_iafront.jobs.pedidos.contants import SAVING_PARTITIONS
+from desafio_iafront.data.dataframe_utils import read_csv, read_partitioned_json
 
 def _prepare(pedidos_joined: pd.DataFrame) -> pd.DataFrame:
     # Remove colunas resultantes do merge
@@ -20,3 +22,46 @@ def drop_merged_columns(data_frame: pd.DataFrame) -> pd.DataFrame:
         if column.endswith("_off"):
             result_dataset = data_frame.drop(column, axis=1)
     return result_dataset
+
+def save_prepared(saida: str, visita_com_produto_e_conversao_df: pd.DataFrame):
+    prepared = _prepare(visita_com_produto_e_conversao_df)
+    save_partitioned(prepared, saida, SAVING_PARTITIONS)
+    return
+
+
+def merge_visita_produto(data_str: str, hour: int, pedidos_df: pd.DataFrame, produtos_df: pd.DataFrame, visitas_df: pd.DataFrame) -> pd.DataFrame:
+    visita_com_produto_df = visitas_df.merge(produtos_df, how="inner", on="product_id", suffixes=("", "_off"))
+    visita_com_produto_e_conversao_df = visita_com_produto_df.merge(pedidos_df, how="left",
+                                                                            on="visit_id", suffixes=("", "_off"))
+    visita_com_produto_e_conversao_df["data"] = data_str
+    visita_com_produto_e_conversao_df["hora"] = hour
+    return visita_com_produto_e_conversao_df
+
+def create_pedidos_df(date_partition: str, hour_snnipet: str, pedidos: pd.DataFrame) -> pd.DataFrame:
+    pedidos_partition = os.path.join(pedidos, date_partition, hour_snnipet)
+    pedidos_df = read_partitioned_json(pedidos_partition)
+    pedidos_df["visit_id"] = pedidos_df["visit_id"].astype(str)
+    return pedidos_df
+
+
+def create_visitas_df(date_partition: str, hour_snnipet: str, visitas: pd.DataFrame) -> pd.DataFrame:
+    visitas_partition = os.path.join(visitas, date_partition, hour_snnipet)
+    visitas_df = read_partitioned_json(visitas_partition)
+    visitas_df["product_id"] = visitas_df["product_id"].astype(str)
+    visitas_df["visit_id"] = visitas_df["visit_id"].astype(str)
+    return visitas_df
+
+def create_save_visita_produto_conversao_df(data: str, hour: int, pedidos: str, produtos_df: pd.DataFrame, saida: str, visitas: str) -> pd.DataFrame:
+    
+    hour_snnipet = f"hora={hour}"
+    data_str = data.strftime('%Y-%m-%d')
+    date_partition = f"data={data_str}"
+
+    visitas_df = create_visitas_df(date_partition, hour_snnipet, visitas)
+
+    pedidos_df = create_pedidos_df(date_partition, hour_snnipet, pedidos)
+
+    visita_com_produto_e_conversao_df = merge_visita_produto(data_str, hour, pedidos_df, produtos_df,visitas_df)
+    save_prepared(saida, visita_com_produto_e_conversao_df)
+
+    return visita_com_produto_e_conversao_df
